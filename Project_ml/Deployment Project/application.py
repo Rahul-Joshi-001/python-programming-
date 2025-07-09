e74be00866c27887f91e0b8ebe6cdfda30f86376
Rahul Joshi <joshirahul3456@gmail.com>
2025-07-09 16:08:04 +0530

    nw

---
+++ Project_ml/Deployment Project/application.py
@@ -0,0 +1,23 @@
+from flask import Flask,request,jsonify,render_template
+import pickle
+import numpy as np
+import pandas as pd
+from sklearn.preprocessing import StandardScaler 
+
+
+application = Flask(__name__)
+app = application
+
+## import ridge regressor and standard scalar pickle
+ridge_model = pickle.load(open('models/ridge.pkl','rb'))
+standard_scalar = pickle.load(open('models/scalar.pkl','rb'))
+
+
+
+@app.route("/")
+def index():
+    return render_template('index.html')
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0")
